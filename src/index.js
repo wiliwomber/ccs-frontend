6b495185d20803c6a3caa4f0ec14395f48d8a9b1
Unknown <mail@rzechlin.de>
2018-06-09 15:18:07 +0200

    new react install, no node modules

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-"use strict";
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import './index.css';
 import App from './App';
-import WebFontLoader from 'webfontloader';
+import registerServiceWorker from './registerServiceWorker';
 
-WebFontLoader.load({
-    google: {
-        families: ['Roboto:300,400,500,700', 'Material Icons'],
-    },
-});
-
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
