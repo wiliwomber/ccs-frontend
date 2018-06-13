841258ea12463f4410a04b9b07648f116edbe2ee
Unknown <mail@rzechlin.de>
2018-06-13 18:32:11 +0200

    Revert "Merge remote-tracking branch 'origin/working_SPr' into newReact"
    This reverts commit e39ef87d7b6b7d385256b28caedbe213c38af13f.

--- src/services/CourseService.js
+++ src/services/CourseService.js
@@ -19,45 +19,45 @@ export default class CourseService {
         });
     }
 
-    static getCourse(id) {
-        return new Promise((resolve, reject) => {
-            HttpService.get(`${CourseService.baseURL()}/${id}`, function(data) {
-                if(data != undefined || Object.keys(data).length !== 0) {
-                    resolve(data);
-                }
-                else {
-                    reject('Error while retrieving Course');
-                }
-            }, function(textStatus) {
-                reject(textStatus);
-            });
-        });
-    }
-
-    static deleteCourse(id) {
-        return new Promise((resolve, reject) => {
-            HttpService.remove(`${CourseService.baseURL()}/${id}`, function(data) {
-                if(data.message != undefined) {
-                    resolve(data.message);
-                }
-                else {
-                    reject('Error while deleting');
-                }
-            }, function(textStatus) {
-                reject(textStatus);
-            });
-        });
-    }
-
-    static updateCourse(course) {
-        return new Promise((resolve, reject) => {
-            HttpService.put(`${this.baseURL()}/${course._id}`, course, function(data) {
-                resolve(data);
-            }, function(textStatus) {
-                reject(textStatus);
-            });
-        });
-    }
+    // static getMovie(id) {
+    //     return new Promise((resolve, reject) => {
+    //         HttpService.get(`${MovieService.baseURL()}/${id}`, function(data) {
+    //             if(data != undefined || Object.keys(data).length !== 0) {
+    //                 resolve(data);
+    //             }
+    //             else {
+    //                 reject('Error while retrieving movie');
+    //             }
+    //         }, function(textStatus) {
+    //             reject(textStatus);
+    //         });
+    //     });
+    // }
+    //
+    // static deleteMovie(id) {
+    //     return new Promise((resolve, reject) => {
+    //         HttpService.remove(`${MovieService.baseURL()}/${id}`, function(data) {
+    //             if(data.message != undefined) {
+    //                 resolve(data.message);
+    //             }
+    //             else {
+    //                 reject('Error while deleting');
+    //             }
+    //         }, function(textStatus) {
+    //             reject(textStatus);
+    //         });
+    //     });
+    // }
+    //
+    // static updateMovie(movie) {
+    //     return new Promise((resolve, reject) => {
+    //         HttpService.put(`${this.baseURL()}/${movie._id}`, movie, function(data) {
+    //             resolve(data);
+    //         }, function(textStatus) {
+    //             reject(textStatus);
+    //         });
+    //     });
+    // }
 
     static createCourse(course) {
         course.id = Math.floor((Math.random() * 100000000) + 1).toString();
