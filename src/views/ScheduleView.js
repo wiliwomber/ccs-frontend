3c024082130fe779df81a7103ae4914d6d60426a
Unknown <mail@rzechlin.de>
2018-06-13 18:07:11 +0200

    Mac Start & Stylesheets

--- src/views/ScheduleView.js
+++ src/views/ScheduleView.js
@@ -3,13 +3,10 @@
 import React from 'react';
 import $ from 'jquery';
 import 'fullcalendar';
-// import './../components/Schedule';
-import './../components/Schedule.css';
+import '../stylesheets/Schedule.css';
 import Popup from 'react-popup';
-import './../components/Popup.css';
+import '../stylesheets/Popup.css';
 import {Snackbar} from 'react-md';
-
-
 
 
 
