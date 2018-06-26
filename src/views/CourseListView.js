a6566f41b4d0741ac57e7805465f0422afb4a956
stefanprobst <probstnet@googlemail.com>
2018-06-26 15:54:06 +0200

    Filter & Search

--- src/views/CourseListView.js
+++ src/views/CourseListView.js
@@ -6,11 +6,15 @@ import { CourseList } from '../components/CourseList';
 
 import CourseService from '../services/CourseService';
 import UserService from "../services/UserService";
-import {DialogContainer, Grid, Cell, Button, SelectField,TextField, FontIcon} from 'react-md';
+import {DialogContainer, Grid, Cell, Button, SelectField,TextField, FontIcon, Slider} from 'react-md';
 import './../App.css';
 import $ from "jquery";
-import Filter from './../components/Filter'
 import { Snackbar } from 'rmwc/Snackbar';
+
+// list of icons that can be used: https://material.io/tools/icons/?icon=android&style=baseline
+
+const NUMBER_ITEMS = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
 
 
 var SnackMessage = "Test";
@@ -28,12 +32,20 @@ export class CourseListView extends React.Component {
             selectedSemester: 'Current Semester',
             course: '',
             searchTerm : '',
+            searchCredits: '',
+            searchSemester: '',
+            searchDay: '',
+            visible : false
         };
 
         //in case a new course is created, the component is updated so that the new course is displayed in the list
         UserService.registerListener("newCourse", this.componentWillMount.bind(this));
         this.handleChangeSelectedSemester = this.handleChangeSelectedSemester.bind(this);
         this.handleChangeSearchTerm = this.handleChangeSearchTerm.bind(this);
+        this.handleChangeSearchCredits = this.handleChangeSearchCredits.bind(this);
+        this.handleChangeSearchSemester = this.handleChangeSearchSemester.bind(this);
+        this.handleChangeSearchDay = this.handleChangeSearchDay.bind(this);
+        this.handleResetFilters = this.handleResetFilters.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.closeForm = this.closeForm.bind(this);
 
@@ -88,6 +100,26 @@ export class CourseListView extends React.Component {
         this.setState({searchTerm : value});
     }
 
+    handleChangeSearchCredits(value){
+        console.log(value);
+        this.setState({searchCredits : value});
+    }
+
+    handleChangeSearchSemester(value){
+        console.log(value);
+        this.setState({searchSemester : value});
+    }
+
+    handleChangeSearchDay(value){
+        console.log(value);
+        this.setState({searchDay : value});
+    }
+    handleResetFilters(){
+        this.setState({searchDay : ''});
+        this.setState({searchSemester : ''});
+        this.setState({searchCredits : ''});
+    }
+
     handleSubmit(){
         this.state.course.selectedSemester = this.state.selectedSemester;
         CourseService.updateCourse(this.state.course).then(
@@ -101,7 +133,17 @@ export class CourseListView extends React.Component {
         this.setState({open: false});
     }
 
+    show = () => {
+        this.setState({ visible: true });
+    };
+
+    hide = () => {
+        this.setState({ visible: false });
+    };
+
     render() {
+        const { visible } = this.state;
+
         if (this.state.loading) {
             return (<h2>Loading...</h2>);
         }
@@ -110,7 +152,52 @@ export class CourseListView extends React.Component {
 
 
         <div>
-            <div><Filter/> </div>
+            <div>
+                <Button raised secondary onClick={this.show}>
+                    Set Filters
+                </Button>
+                <DialogContainer
+                    id="course-list-filter"
+                    visible={visible}
+                    onHide={this.hide}
+                    title="Filters"
+
+                >
+                    <SelectField
+                        id="select-field-1"
+                        lable="Day"
+                        placeholder=""
+                        className="md-cell"
+                        menuItems={DAYS}
+                        onChange={this.handleChangeSearchDay}
+                        position={SelectField.Positions.TOP_RIGHT}
+                        leftIcon={<FontIcon>calendar_today</FontIcon>}/>
+                    <Slider
+                        id="semester_slider"
+                        label="Semester"
+                        leftIcon={<FontIcon>hourglass_empty</FontIcon>}
+                        onChange={this.handleChangeSearchSemester}
+                        defaultValue={5}
+                        max={10}
+                        discrete
+                    />
+                    <Slider
+                        id="credit_slider"
+                        label="Credits"
+                        leftIcon={<FontIcon>school</FontIcon>}
+                        onChange={this.handleChangeSearchCredits}
+                        defaultValue={5}
+                        max={10}
+                        discrete
+                    />
+
+                </DialogContainer>
+            </div>
+            <div>
+                <Button onClick={this.handleResetFilters}>
+                    Reset all filters
+                </Button>
+            </div>
             <div>
                 <TextField
                     id="search_field"
@@ -124,7 +211,7 @@ export class CourseListView extends React.Component {
                 />
             </div>
 
-            <CourseList data={this.state.data} searchTerm={this.state.searchTerm} height={$(window).height()} onAdd={(id) => this.chooseCourse(id)}/>
+            <CourseList data={this.state.data} searchTerm={this.state.searchTerm} searchCredits={this.state.searchCredits} searchSemester={this.state.searchSemester} searchDay={this.state.searchDay} height={$(window).height()} onAdd={(id) => this.chooseCourse(id)}/>
 
             <DialogContainer
                 component={'MainPageView'}
