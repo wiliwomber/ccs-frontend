c7248e50b410d3f0a4c1479c5f8127565cd03ea6
ewolz <ga92jig@mytum.de>
2018-07-01 23:39:37 +0200

    test

--- src/views/ScheduleView.js
+++ src/views/ScheduleView.js
@@ -18,6 +18,7 @@ export class ScheduleView extends React.Component {
         this.state = {
             popup : '',
             course: '',
+            
           //  open: false,
         };
 
